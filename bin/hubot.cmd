f2072294203fb61dd5e5cd5be3bc98c786773a82
Eric Carraway <eric.carraway@uicentric.com>
2017-02-14 09:52:30 -0600

    billing.bot.thornton -> coach_botovich

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,4 +4,4 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "billing.bot.thornton" %* 
+node_modules\.bin\hubot.cmd --name "coach_botovich" %* 
